674c063db888457b551bb5ee44a3de4bd25c29e0
zhangchunsheng <zhangchunsheng423@gmail.com>
2022-06-16 16:21:48 +0800

    update

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ..
+git checkout feature/luomor
 npm run build:prod
 cd ai-web
 rm -rf webroot/*
@@ -7,3 +8,5 @@ cp -r ../dist/* webroot
 git add .
 git commit -a -m 'update'
 git push -u origin main
+cd ..
+git checkout main
